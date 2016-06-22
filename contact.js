35a6e7416cef7352e7cb55f03a3acfc5f09ef1e1
daniel gadot <danielgadot@gmail.com>
2016-06-22 15:44:41 +0300

    added checkform

---
+++ contact.js
@@ -0,0 +1,8 @@
+function checkForm(){
+    var fName = document.querySelector('#firstname').value;
+    var secondName = document.querySelector('#secondname').value;
+    var fullName = fName + secondName;
+    // console.log('fullname',fullName);
+    
+    
+}
